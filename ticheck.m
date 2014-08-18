ff20bb7513f50044a69137706890d1637d6cefb6
Taylor Page <taylorrpage@gmail.com>
2014-08-18 17:39:24 -0500

    Update ticheck help

--- ticheck.m
+++ ticheck.m
@@ -1,4 +1,6 @@
 function [t, tcheck] = ticheck(k1, t)
+%Checks if the first time point is early enough for an accurate numerical
+%solution given a decay constant.
 
 tcheck = false;
 ti = log(1/k1) - 2;
